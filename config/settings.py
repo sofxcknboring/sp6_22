a3c3a51281e42122394acd603becf7318e9ffce7
v.mishurenko <paranoiawe@gmail.com>
2024-12-16 06:07:41 +0300

    add static files

--- config/settings.py
+++ config/settings.py
@@ -81,6 +81,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIR = (BASE_DIR / 'static',)
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
