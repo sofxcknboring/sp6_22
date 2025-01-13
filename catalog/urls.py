32f36a88167dbfc587068d21e8940bfe5e60f5ac
v.mishurenko <paranoiawe@gmail.com>
2025-01-13 05:22:24 +0300

    add forms and crud for catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, ContactTemplateView
+from catalog.views import (
+    ProductListView,
+    ProductDetailView,
+    ContactTemplateView,
+    ProductCreateView,
+    ProductDeleteView,
+    ProductUpdateView,
+)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
-    path('contact/', ContactTemplateView.as_view(), name='contact'),
+    path("contact/", ContactTemplateView.as_view(), name="contact"),
     path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path(
+        "product_delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"
+    ),
+    path("product_create/", ProductCreateView.as_view(), name="product_create"),
+    path(
+        "product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"
+    ),
 ]
